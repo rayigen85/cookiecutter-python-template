286b7619f38fa9f015bd034c592b91ca4510047f
rayigen85 <raymondbaruth@raybit.de>
2022-07-25 15:25:57 +0200

    Update setup.py

--- {{ cookiecutter.distribution_name }}/setup.py
+++ {{ cookiecutter.distribution_name }}/setup.py
@@ -17,7 +17,7 @@ setup(
     classifiers=(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: CPython',
     )
 )
